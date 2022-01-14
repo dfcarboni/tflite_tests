da8b6d1ff63231cc0515cf403e8abcfeb16d13c1
dfcarboni <97053311+dfcarboni@users.noreply.github.com>
2022-01-14 13:36:56 -0300

    updated inference scripts

--- tflite_imageclassification_script_f32.py
+++ tflite_imageclassification_script_f32.py
@@ -3,9 +3,7 @@
 Computer vision team
 """
 # Import packages
-import os
 import argparse
-# import cv2
 import numpy as np
 import time
 from PIL import Image
@@ -15,16 +13,23 @@ parser = argparse.ArgumentParser()
 parser.add_argument('--model', help='model.tflite path',
                     required=True)
 parser.add_argument('--image', help='Path of image to be inferenced',
-                    default=None)
+                    required=True)
 parser.add_argument('--img_size', help='Image size for model',
-                    default=(224,224))
+                    nargs=2, default= (224,224))
+parser.add_argument('--grayscale', help='convert img to grayscale',
+                    action='store_true')
 
 args = parser.parse_args()
 
 MODEL = args.model
 IMAGE_PATH = args.image
-IMG_SIZE= args.img_size #not working xD
+IMG_SIZE= args.img_size
+grayscale= args.grayscale
+
+W= int(IMG_SIZE[0])
+H= int(IMG_SIZE[1])
 # print(IMG_SIZE)
+
 #------------------------------------------------------------------------------
 #Load TFlite Model  
 import tflite_runtime.interpreter as tflite
@@ -39,20 +44,28 @@ input_details = interpreter.get_input_details()
 output_details = interpreter.get_output_details()
 
 #Inference
-input_data = Image.open(IMAGE_PATH) 
-input_data= input_data.resize(IMG_SIZE, Image.ANTIALIAS)
-input_data= np.array(input_data, dtype=np.uint8).reshape(-1,IMG_SIZE[0],IMG_SIZE[1],3)
+if grayscale:    
+    input_data = Image.open(IMAGE_PATH).convert('L') 
+    input_data= input_data.resize((W,H), Image.ANTIALIAS)
+    input_data= np.array(input_data, dtype=np.float32).reshape(-1,W,H,1)
+else: 
+    input_data = Image.open(IMAGE_PATH) 
+    input_data= input_data.resize((W,H), Image.ANTIALIAS)
+    input_data= np.array(input_data, dtype=np.float32).reshape(-1,W,H,3)
+
+
 interpreter.set_tensor(input_details[0]['index'], input_data)
 
-start_time = time.monotonic()
+start_time = time.perf_counter()
 interpreter.invoke()
-elapsed_ms = (time.monotonic() - start_time) * 1000
+elapsed_ms = (time.perf_counter() - start_time) * 1000
 
-print(f'Inference Time: {np.round_(elapsed_ms,2)} ms')
+print('Inference Time: {:.2f} ms'.format(elapsed_ms))
 
 output_data = interpreter.get_tensor(output_details[0]['index'])
 
-print("Sem daninha" if int(output_data) > 127 else "Com daninha")
+# print("Sem daninha" if int(output_data) > 127 else "Com daninha")
+print(output_data)
 #---------------------------------------------------------------
 
 #functions
---
+++ tflite_imageclassification_script_int8.py
@@ -0,0 +1,74 @@
+"""
+2021 Eirene Solutions
+Computer vision team
+"""
+# Import packages
+import argparse
+import numpy as np
+import time
+from PIL import Image
+
+# Define and parse input arguments
+parser = argparse.ArgumentParser()
+parser.add_argument('--model', help='model.tflite path',
+                    required=True)
+parser.add_argument('--image', help='Path of image to be inferenced',
+                    required=True)
+parser.add_argument('--img_size', help='Image size for model',
+                    nargs=2, default= (224,224))
+parser.add_argument('--grayscale', help='convert img to grayscale',
+                    action='store_true')
+
+args = parser.parse_args()
+
+MODEL = args.model
+IMAGE_PATH = args.image
+IMG_SIZE= args.img_size
+grayscale= args.grayscale
+
+W= int(IMG_SIZE[0])
+H= int(IMG_SIZE[1])
+# print(IMG_SIZE)
+
+#------------------------------------------------------------------------------
+#Load TFlite Model  
+import tflite_runtime.interpreter as tflite
+
+# Load the TFLite model and allocate tensors.
+interpreter = tflite.Interpreter(model_path=MODEL)
+interpreter.allocate_tensors()
+model= interpreter
+
+# Get input and output tensors.
+input_details = interpreter.get_input_details()
+output_details = interpreter.get_output_details()
+
+#Inference
+if grayscale:    
+    input_data = Image.open(IMAGE_PATH).convert('L') 
+    input_data= input_data.resize((W,H), Image.ANTIALIAS)
+    input_data= np.array(input_data, dtype=np.uint8).reshape(-1,W,H,1)
+else: 
+    input_data = Image.open(IMAGE_PATH) 
+    input_data= input_data.resize((W,H), Image.ANTIALIAS)
+    input_data= np.array(input_data, dtype=np.uint8).reshape(-1,W,H,3)
+
+
+interpreter.set_tensor(input_details[0]['index'], input_data)
+
+start_time = time.perf_counter()
+interpreter.invoke()
+elapsed_ms = (time.perf_counter() - start_time) * 1000
+
+print('Inference Time: {:.2f} ms'.format(elapsed_ms))
+
+output_data = interpreter.get_tensor(output_details[0]['index'])
+
+# print("Sem daninha" if int(output_data) > 127 else "Com daninha")
+print(output_data)
+#---------------------------------------------------------------
+
+#functions
+
+#Example
+#python3 tflite_detect_script.py --model weeds.tflite --image img_1597293167.46.png
